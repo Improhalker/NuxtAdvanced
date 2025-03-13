79ab66dd49b344f1e89c8dde551424337e55330b
Improhalker <gabrielestrela1009@hotmail.com>
2025-03-13 12:00:23 -0300

    Fix Remove outdated blog posts and implement new blog index page with content fetching

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const { data: home, error } = await useAsyncData(() => queryCollection('content').path('/blog').first())
+
+if (error.value) {
+    console.error('Failed to fetch home content:', error.value)
+}
+
+useSeoMeta({
+    title: home.value?.title || 'Default Title',
+    description: home.value?.description || 'Default Description'
+})
+</script>
+
 <template>
-    <h1>
-        The blog section, give a id parameter to the URL
-    </h1>
+    <ContentRenderer v-if="home && !error" :value="home" />
+    <div v-else-if="error">Failed to load home content</div>
+    <div v-else>Home not found</div>
 </template>
