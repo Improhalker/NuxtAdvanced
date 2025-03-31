7e107e8d694d40411285024dcd4143b8a7d28531
Gabriel Estrela <149545964+Improhalker@users.noreply.github.com>
2025-03-31 19:50:28 -0300

    Fix: Dark Mode

--- components/content/project-list.vue
+++ components/content/project-list.vue
@@ -4,7 +4,7 @@
     <section v-else-if="error">Someting went wrong... Try again!</section>
     <section v-else>Here we display the repos</section>
     <ul class="grid grid-cols-1 gap-4"> 
-        <li v-for="repository in repos" :key="repository.id" class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 dark:hover:bg-gray-500 font-mon">
+        <li v-for="repository in repos" :key="repository.id" class="border border-gray-200 dark:border-gray-700 rounded-sm p-4 hover:bg-gray-200 dark:hover:bg-gray-800 font-mon">
             <a :href="repository.html_url" target="_blank">
                 <div class="flex items-center justify-between">
                     <div class="font-semibold">{{ repository.name }}</div>
--- components/menu.vue
+++ components/menu.vue
@@ -2,16 +2,16 @@
     <nav class="font-mono">
         <ul class="flex space-x-4">
             <li>
-                <NuxtLink to="/" class="link">Main</NuxtLink>
+                <NuxtLink to="/" class="link dark:hover:bg-gray-800">Main</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/about" class="link">About</NuxtLink>
+                <NuxtLink to="/about dark:hover:bg-gray-800" class="link">About</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/projects/" class="link">Projects</NuxtLink>
+                <NuxtLink to="/projects/" class="link dark:hover:bg-gray-800" >Projects</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/blog/" class="link">Blog</NuxtLink>
+                <NuxtLink to="/blog/" class="link dark:hover:bg-gray-800">Blog</NuxtLink>
             </li>
         </ul>
     </nav>
