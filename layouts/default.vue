e63e9e7ada14491bd85e7b23570811114359179e
Gabriel Estrela <149545964+Improhalker@users.noreply.github.com>
2025-04-29 20:00:36 -0300

    Add footer section and social media icons to default layout; create button component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,61 @@
 <template>
     <div class="">
-        <header class="flex justify-between items-start md:items-center mt-5">
+        <header class="flex justify-between items-start md:items-center">
             <div class="flex items-center md:space-x-12">
                 <div class="hidden md:block">
-                    <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200  dark:hover:bg-gray-800">Gabriel Estrela</NuxtLink>
+                    <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200  dark:hover:bg-gray-800">Gabriel
+                        Estrela</NuxtLink>
                 </div>
-        
+
                 <Menu />
             </div>
             <ColorModeSelector />
         </header>
 
         <main>
+            <slot />
+        </main>
 
-       
-        <slot/>
-    </main>
+        <footer>
+            <div class="w-full h-56 bg-emphasis flex items-center justify-center">
+                <div class="p-8 w-3/4 h-5/6 flex flex-col md:flex-row justify-between items-center gap-6">
+                    <div class="text-center md:text-left">
+                        <h3 class="text-xl font-bold text-white">GABRIEL ESTRELA</h3>
+                        <p class="text-sm text-gray-200 max-w-md">
+                            A security enthusiast that started your career focusing in Web Applications
+                        </p>
+                    </div>
+
+                    <div class="flex flex-col items-center md:items-end">
+                        <h3 class="text-xl place-self-center font-bold text-white mb-2">SOCIAL</h3>
+                        <div class="flex gap-4">
+                            <a href="#" aria-label="LinkedIn">
+                                <img src="/images/linkedin-ico.png" alt="LinkedIn" class="h-6 w-6" />
+                            </a>
+                            <a href="#" aria-label="GitHub">
+                                <img src="/images/github-ico.png" alt="GitHub" class="h-6 w-6" />
+                            </a>
+                            <a href="#" aria-label="Mail">
+                              <img src="/images/icons8-mail-50.png" alt="Twitter" class="h-6 w-6" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
 <script setup>
 
-    useHead({
+useHead({
     titleTemplate: '%s - Gabriel Estrela'
 })
 
 </script>
 
 <style>
-    body { @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300} 
-
+body {
+    @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
         <section class="bg-gradient-to-br from-[#abf8ea] via-[#54306e] to-[#253342] dark:from-surface dark:to-background p-8 mt-8 text-center h-[90vh] flex flex-col justify-center items-center 
          animate-gradient-slow bg-[length:200%_200%]">
 
-            <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold text-white dark:text-gray-900 mb-6">I'm Gabriel Estrela</h1>
+            <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold text-white dark:text-gray-900 mb-6">I'm Gabriel
+                Estrela</h1>
             <h2 class="text-3xl sm:text-4xl lg:text-5xl font-bold text-surface dark:text-purple-600 mb-6">
                 Building the web, <span class="text-yellow-400 dark:text-accentHover">Securing the future</span>
             </h2>
@@ -30,11 +31,11 @@
                 programming and technolgy knowledge!
             </p>
 
-            <div class="flex justify-center mt-2 sm:mt-4 md:mt-8 gap-8">
-                <div class="text-start mt-2 sm:mt-4 md:mt-8 gap-8">
+            <div class="flex flex-wrap w-3/4 place-self-center justify-center mt-2 sm:mt-4 md:mt-8 gap-4">
+                <div class="sm:text-start mt-4 md:mt-8 gap-8">
                     <h2 class="text-2xl font-bold text-gray-800 dark:text-white">Let you know me!</h2>
 
-                    <p class="text-lg  text-gray-600 dark:text-gray-300 mt-2  max-w-2xl">
+                    <p class="text-lg text-gray-600 dark:text-gray-300 mt-2 max-w-2xl">
                         I am currently working at Postali Marketing Digital, performing Full Stack tasks. On the front
                         end,
                         I
@@ -44,8 +45,8 @@
                         the expertise to start new projects from scratch as well.
                     </p>
                 </div>
-                <div class="w-full max-w-2xl mt-2 sm:mt-4 md:mt-8 gap-8 ">
-                    <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-4 text-center">My Skills</h2>
+                <div class="w-[500px] mt-2 sm:mt-4 md:mt-8 gap-8 ">
+                    <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-4 text-start">My Skills</h2>
                     <ul class="grid grid-cols-2 sm:grid-cols-3 gap-6 text-lg text-gray-600 dark:text-gray-300">
                         <li v-for="skill in skills" :key="skill"
                             class="bg-gray-100 dark:bg-gray-700 p-4 rounded shadow transition duration-300 transform hover:scale-105 hover:bg-accent hover:text-white dark:hover:bg-accentHover text-center">
